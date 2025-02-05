1d6d07938f68669f8fdf14fa0bf114494afcdcc5
Mikhail Doshevsky <m.doshevsky@gmail.com>
2025-02-05 18:55:20 +0300

    Add tasks code

---
+++ src/task-05/yandex-art-api.py
@@ -0,0 +1,46 @@
+import os
+import time
+from io import BytesIO
+
+import requests
+from datetime import datetime
+from base64 import b64decode
+from PIL import Image
+
+from dotenv import load_dotenv
+
+load_dotenv()
+folder_id = os.getenv("YANDEX_FOLDER_ID")
+api_key = os.getenv("YANDEX_API_KEY")
+
+seed = int(round(datetime.now().timestamp()))
+prompt = "Милый пушистый котенок спит на спине. Octane render,f/2.8, ISO 200"
+
+body = {
+    "modelUri": f"art://{folder_id}/yandex-art/latest",
+    "generationOptions": {"seed": seed, "temperature": 0.6},
+    "messages": [
+        {"weight": 1, "text": prompt},
+    ],
+}
+url = "https://llm.api.cloud.yandex.net/foundationModels/v1/imageGenerationAsync"
+headers = {"Authorization": f"Api-Key {api_key}"}
+
+response = requests.post(url, headers=headers, json=body)
+operation_id = response.json()["id"]
+
+url = f"https://llm.api.cloud.yandex.net:443/operations/{operation_id}"
+headers = {"Authorization": f"Api-Key {api_key}"}
+
+while True:
+    response = requests.get(url, headers=headers)
+    done = response.json()["done"]
+    if done:
+        break
+    else:
+        time.sleep(2)
+
+image_data = response.json()["response"]["image"]
+image_bytes = b64decode(image_data)
+image = Image.open(BytesIO(image_bytes))
+image.show()
